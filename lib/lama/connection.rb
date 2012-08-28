4293328e83d0930d4aa6b00530ead1810f7b49a4
Amir Reavis-Bey <amirbey@gmail.com>
2012-08-28 15:51:53 -0700

    updated faraday connection

--- lib/lama/connection.rb
+++ lib/lama/connection.rb
@@ -14,14 +14,27 @@ module LAMA
         :url => endpoint
       })
 
-      Faraday.new(merged_options) do |builder|
-        builder.use Faraday::Connection::basic_auth login, pass
-        builder.use Faraday::Request::UrlEncoded
-        builder.use Faraday::Response::RaiseError
-        builder.use FaradayMiddleware::Mashify
-        builder.use FaradayMiddleware::ParseXml
-        builder.adapter(Faraday.default_adapter)
+      ## in Faraday 0.8 or above:
+      connection = Faraday.new(merged_options) do |conn|
+        conn.request :oauth2, 'TOKEN'
+        conn.request :json
+
+        conn.response :xml,  :content_type => /\bxml$/
+        conn.response :json, :content_type => /\bjson$/
+
+        conn.use :instrumentation
+        conn.adapter Faraday.default_adapter
       end
+
+## with Faraday 0.7:
+      # Faraday.new(merged_options) do |builder|
+      #   builder.use Faraday::Connection::basic_auth login, pass
+      #   builder.use Faraday::Request::UrlEncoded
+      #   builder.use Faraday::Response::RaiseError
+      #   builder.use FaradayMiddleware::Mashify
+      #   builder.use FaradayMiddleware::ParseXml
+      #   builder.adapter(Faraday.default_adapter)
+      # end
     end
   end
 end
